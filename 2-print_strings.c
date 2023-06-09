5b7fa908a15d29fe9debf1c786c98fbc9ed415b4
AndersonRamirez037 <andersongamer037@gmail.com>
2023-06-09 07:24:12 -0500

    2-print_strings.c

---
+++ 2-print_strings.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * 
+ * 
+ * 
+ */
+void print_strings(const char *separator, const unsigned int n, ...){
+    va_list args;
+    va_start(args, n);
+    
+}
